91a183f45030463cbe8326bba67c2146cecfe29c
Himanshu Tiwary <HIMANSHU.23SCSE1011208@galgotiasuniversity.ac.in>
2025-01-26 22:24:12 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,29 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+from os import environ 
+
+class Config:
+    API_ID = int(environ.get("API_ID", "5625170"))
+    API_HASH = environ.get("API_HASH", "77633c2757f8697650e0c31cf505ffbc")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7781812513:AAHPvxb5_KBNSm8fY_mR_zUiOkPhdMZid9w") 
+    BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://himanshu12feb2k23:JaU8qjZ53Q8mWbP0@cluster0.keoec.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
+    BOT_OWNER = int(environ.get("BOT_OWNER", "5723551431"))
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+class temp(object): 
+    lock = {}
+    CANCEL = {}
+    forwardings = 0
+    BANNED_USERS = []
+    IS_FRWD_CHAT = []
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
